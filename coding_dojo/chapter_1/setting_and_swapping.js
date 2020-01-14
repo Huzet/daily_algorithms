e0841722b3e531b0a6e469cf2240598da15e0385
Tomas <tomas.huzevka@outlook.com>
2020-01-13 22:30:26 -0500

    one

---
+++ coding_dojo/chapter_1/setting_and_swapping.js
@@ -0,0 +1,9 @@
+// Setting and Swapping 
+// Set myNumber to 42. Set myName to your name. Now swap myNumber into myName &vice versa.
+
+let mynumber = 42;
+let myname = "Tomas";
+let temp = mynumber;
+mynumber = myname;
+myname = temp;
+console.log(mynumber, myname)
